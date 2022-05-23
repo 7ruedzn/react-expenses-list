60e3c3b5b66bf672dcab5595f4ac67aa3fd8e07a
Vinícius <iits7rue@gmail.com>
2022-05-23 19:08:53 -0300

    useState para atualizar título do produto (testing)

--- src/components/ExpenseItem/ExpenseItem.tsx
+++ src/components/ExpenseItem/ExpenseItem.tsx
@@ -1,9 +1,4 @@
-import {
-  ReactElement,
-  JSXElementConstructor,
-  ReactFragment,
-  ReactPortal,
-} from "react";
+import { useState } from "react";
 import "./ExpenseItem.scss";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import Card from '../Card/Card'
@@ -15,14 +10,22 @@ interface Props {
 }
 
 const ExpenseItem = (props: Props) => {
+
+  const [title, setTitle] = useState(props.title)
+
+  const clickHandler = () => {
+    setTitle('updated!!!')
+  }
+
   return (
       <Card>
         <div className="expense-item">
-        <ExpenseDate date={props.date} />
-        <div className="name-price">
-            <h2 className="name">{props.title}</h2>
-            <div className="price">${props.price}</div>
-        </div>
+          <ExpenseDate date={props.date} />
+          <div className="name-price">
+              <h2 className="name">{title}</h2>
+              <div className="price">${props.price}</div>
+          </div>
+          <button onClick={clickHandler}>oioioi</button>
         </div>
       </Card>
   );
